05de3a91c07f9a2e328b4158760bbd84697005d6
Wassim Tahraoui <wassim.tahraoui88@gmail.com>
2025-11-29 22:39:03 +0100

    - Added FileType enum and enhance TXFileUtils with file type detection.

---
+++ src/main/java/com/tahraoui/txcore/io/FileType.java
@@ -0,0 +1,7 @@
+package com.tahraoui.txcore.io;
+
+import java.io.Serializable;
+
+public enum FileType implements Serializable {
+	IMAGE, AUDIO, VIDEO, DOC, ARCHIVE, OTHER
+}
--- src/main/java/com/tahraoui/txcore/io/TXFileUtils.java
+++ src/main/java/com/tahraoui/txcore/io/TXFileUtils.java
@@ -9,10 +9,54 @@ import java.io.ObjectOutputStream;
 
 public class TXFileUtils {
 
+	private static final String
+			EXTENSION_JPEG = "jpeg", EXTENSION_JPG = "jpg", EXTENSION_PNG = "png", EXTENSION_GIF = "gif", EXTENSION_BMP = "bmp",
+			EXTENSION_MP3 = "mp3", EXTENSION_WAV = "wav", EXTENSION_OGG = "ogg",
+			EXTENSION_MP4 = "mp4", EXTENSION_AVI = "avi", EXTENSION_MKV = "mkv",
+			EXTENSION_PDF = "pdf", EXTENSION_DOCX = "docx", EXTENSION_TXT = "txt",
+			EXTENSION_ZIP = "zip", EXTENSION_RAR = "rar", EXTENSION_TAR = "tar";
+
+	public static String concatPaths(String... paths) {
+		var separator = "/";
+		var sb = new StringBuilder();
+		for (var path : paths) {
+			if (!sb.isEmpty() && !sb.toString().endsWith(separator)) sb.append(separator);
+			sb.append(path.startsWith(separator) ? path.substring(1) : path);
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * Determines the FileType based on the file extension.
+	 * @param filename The name of the file.
+	 * @return The corresponding FileType.
+	 */
+	public static FileType getFileType(String filename) {
+		var extension = getFileExtension(filename);
+		return switch (extension) {
+			case EXTENSION_JPEG, EXTENSION_JPG, EXTENSION_PNG, EXTENSION_GIF, EXTENSION_BMP -> FileType.IMAGE;
+			case EXTENSION_MP3, EXTENSION_WAV, EXTENSION_OGG -> FileType.AUDIO;
+			case EXTENSION_MP4, EXTENSION_AVI, EXTENSION_MKV -> FileType.VIDEO;
+			case EXTENSION_PDF, EXTENSION_DOCX, EXTENSION_TXT -> FileType.DOC;
+			case EXTENSION_ZIP, EXTENSION_RAR, EXTENSION_TAR -> FileType.ARCHIVE;
+			default -> FileType.OTHER;
+		};
+	}
+	/**
+	 * Extracts the file extension from the given filename.
+	 *
+	 * @param filename The filename to extract the extension from.
+	 * @return The file extension in lowercase, or an empty string if none exists.
+	 */
+	public static String getFileExtension(String filename) {
+		int lastIndex = filename.lastIndexOf('.');
+		if (lastIndex == -1 || lastIndex == filename.length() - 1) return "";
+		return filename.substring(lastIndex + 1).toLowerCase();
+	}
+
 	/**
 	 * Checks if the given file has the specified extension.
-	 *
-	 * @param file      The file to check.
+	 * @param file The file to check.
 	 * @param extension The extension to verify (without the dot).
 	 * @return true if the file ends with the specified extension, false otherwise.
 	 */
